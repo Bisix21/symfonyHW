ebf8018163753763b580659f192f711d18656de9
Fenix-25 <serqdrago@gmail.com>
2023-06-21 17:49:46 +0300

    added 2 routs

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -12,7 +12,13 @@ class IndexController extends AbstractController
     public function index(): Response
     {
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'username' => $username ??  "Guest ",
         ]);
     }
+
+	#[Route('/short')]
+	public function shortPlug()
+	{
+		return $this->render('index/plug.html.twig');
+	}
 }
